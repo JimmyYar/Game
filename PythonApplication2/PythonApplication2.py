5e9ab5bc33c7ebf3b7335e84f4d665194572afa6
Jimmy <jimmybee@vk.com>
2022-06-26 15:19:14 +0300

    -Some bugs fixed -Class Business now working well -Function Start now don't looping -Added Cheat - 113

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,11 +1,10 @@
 Player = ""
-Bank = 0
+Stop = True
 
 from datetime import datetime
 from threading import Thread
 import os
 import time
-import keyboard
 
 clean = lambda: os.system('cls')
 
@@ -27,14 +26,15 @@ class Person:
          self.balance = 0
          self.work = "none"
          self.income = 0
+         self.dps = 0
+         self.business = 0
 
-     def show(self): return f"  {self.name}, {self.age} years, Balance: {self.balance}$, Work: {self.work}\n"
+     def show(self): return f"  {self.name}, {self.age} years, Balance: {self.balance}$, Work: {self.work}, Dps: {self.dps}$\n" # Person
 
 class Work:
 
     def __init__(self):
-        start = 1
-        while start != 0:
+        while True:
             try:
                 clean()
                 print(Player.show())
@@ -65,13 +65,12 @@ class Work:
         Player.balance += Player.income
 
     def up(self):
-        Player.income = round(Player.income + Player.income / 10)
+        Player.income = round(Player.income + Player.income / 10) # Work
 
 class Settings:
     def __init__(self):
         clean()
-        start = 1
-        while start != 0:
+        while True:
             print(f"1 - Recreate person\n2 - Edit person\n3 - Choose work\n4 - Save\n5 - Load\n6 - Close")
             try:
                 choiceS = int(input())
@@ -94,7 +93,7 @@ class Settings:
                         break
                 break
             except:
-                clean
+                clean()
                 break
 
     def CreatePerson():
@@ -143,7 +142,7 @@ class Settings:
 
     def Save():
         save = open("save.txt", "w")
-        save.write(f"{Player.name}*{Player.age}*{Player.balance}*{Player.work}*{Player.income}")
+        save.write(f"{Player.name}*{Player.age}*{Player.balance}*{Player.work}*{Player.income}*{Player.dps}*{Player.business}")
         save.close()
 
     def Load():
@@ -158,10 +157,14 @@ class Settings:
             Player.balance = int(data[2])
             Player.work = str(data[3])
             Player.income = int(data[4])
+            Player.dps = int(data[5])
+            Player.business = int(data[6])
+            if Player.business == 1:
+                M.start()
             load.close()
         except:
             clean()
-            Start()
+            return 0 # Settings
 
 def TimeNow():
     now = datetime.now()
@@ -169,61 +172,94 @@ def TimeNow():
     minut = int("{}".format(now.minute))
     chas = int("{}".format(now.hour))
     sec = sec + minut * 60 + chas * 3600
-    return sec
+    return sec # TimeNow
 
 class Business():
-    def __init__():
-        pass
-    
-    def bisn():
-        global Bank
+    def __init__(self):
         while True:
-            Bank += 3
-            time.sleep(1)
+            try:
+                clean()
+                print(Player.show())
+                choiceM = int(input(f"1 - Open\n2 - Buy cafe (10$/sec)\n3 - Buy shop (23$/sec)\n4 - Close\n"))
+                match choiceM:
+                    case 1:
+                        if Player.business == 0:
+                            M.start()
+                            Player.business = 1
+                    case 2:
+                        self.Cafe()
+                    case 3:
+                        self.Shop()
+                    case 4:
+                        break
+            except:
+                continue
+    
+    def Cafe(self): Player.dps += 10
 
-    def Take():
-        global Bank
-        Player.balance += Bank
-        Bank = 0
+    def Shop(self): Player.dps += 23
+    
+    def open():
+        if Player.business == 1:
+            M.start()
+
+    def inc():
+        global Stop
+        while Stop:
+            Player.balance += Player.dps
+            time.sleep(1) # Business
 
 def Start():
-    try:
-        check = int(input("Load save? (0 | 1)\n"))
-    except:
-        clean()
-        Start()
-    if check == 1:
-        Settings.Load()
-        Menu()
-    elif check == 0:
-        Settings.CreatePerson()
-        Menu()
-    else:
-        clean()
-        Start()
+    while True:
+        try:
+            check = int(input("Load save? (0 | 1)\n"))
+        except:
+            clean()
+            continue
+        if check == 1:
+            if Settings.Load() == 0:
+                continue
+            else:
+                Menu()
+            return
+        elif check == 0:
+            Settings.CreatePerson()
+            Menu()
+            return
+        else:
+            clean()
+            continue # Start
 
 def Menu():
-    start = 1
-    while start != 0:
+    global Stop
+    while True:
         try:
             clean()
             print(Player.show())
-            choiceM = int(input(f"1 - Work\n2 - Business({Bank}$)\n3 - Settings\n4 - Exit\n5 - Take money from business\n"))
+            choiceM = int(input(f"1 - Work\n2 - Business\n3 - Settings\n4 - Exit\n"))
         except:
             continue
         match choiceM:
             case 1:
                 Work()
             case 2:
-                M = Thread(target = Business.bisn)
-                M.start()
+                Business()
             case 3:
                 Settings()
             case 4:
                 Settings.Save()
+                Stop = False
                 break
-            case 5:
-                Business.Take()
+            case 113:
+                Player.name = "*CHEATER*"
+                Player.age = 69
+                Player.balance = 143
+                Player.work = "*CHEATER*"
+                Player.income = 666666
+                Player.dps = 777777
+                Player.business = 1
+                Business.open() # Menu
 
 Starting = Thread(target = Start)
+M = Thread(target = Business.inc)
 Starting.start()
